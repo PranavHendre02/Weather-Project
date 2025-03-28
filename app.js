12905742a6c83bbd446559818e6bfae8fa6e8dbd
Pranav <pranavhendre07@gmail.com>
2025-03-28 19:06:38 +0530

    updated the port value

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 const IS_RENDER = process.env.RENDER || "false"; // Ensure it's a string comparison
 
 let sensorData = { temperature: "--", humidity: "--", heatIndex: "--" };
